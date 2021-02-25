445ad84d69294c4cf648027f48b99cefdcd56595
alain17-web <web2020.alain@gmail.com>
2021-02-25 19:41:13 +0100

    first commit

---
+++ notes.js
@@ -0,0 +1,20 @@
+// Dans l'array toDo un valeur true signifie que cette action est accomplie, une valeur false signifie qu'elle est à faire
+let notesJSON = [
+	{
+		"titre": "Notes Javascript",
+		"texte": "Une méthode est une fonction appartenant à un objet",
+		"date": "15/01/2021",
+		"couleur": "rouge",
+		"dateRappel": "null"
+	},
+	{
+		"titre": "Notes Javascript",
+		"toDo": [
+			{"Faire les courses": true},
+			{"Payer le loyer": false}
+		],
+		"date": "18/01/2021",
+		"couleur": "rouge",
+		"dateRappel": "null"
+	}
+]
